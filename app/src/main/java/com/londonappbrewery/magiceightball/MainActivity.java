5d5ffa66b54006ceb82c7f29935560cee3b4f85d
William <william@skoglycke.se>
2017-12-17 22:44:22 +0100

    Project complete!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+
+        Button myButton = (Button) findViewById(R.id.askButton);
+
+        final int[] ballArray = new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5};
+
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // This code gets executed when the button is pressed
+
+                // Create a random number generator
+                Random randomNumberGenerator = new Random();
+
+                // Make the random number generator spit out a new random number
+                int number = randomNumberGenerator.nextInt(5);
+
+                // grab a random dice image from the diceArray. Then tell the ImageView to display
+                // this image
+                ballDisplay.setImageResource(ballArray[number]);
+
+                Log.d("magiceightball", "Random number is: " +number);
+
+            }
+        });
+
+
     }
 }
